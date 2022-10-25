1704dc84f98a25c640c97a2d8dd652be187491c6
Tim Kolberger <tim@kolberger.eu>
2022-10-25 16:32:54 +0400

    feat: add styled system property colorPalette

--- packages/components/styled-system/src/config/color.ts
+++ packages/components/styled-system/src/config/color.ts
@@ -1,12 +1,19 @@
 import * as CSS from "csstype"
-import { Config } from "../utils/prop-config"
+import { Config, PropConfig } from "../utils/prop-config"
 import { t, Token } from "../utils"
+
+const colorPalettePropConfig: PropConfig = {
+  transform(value, theme) {
+    return theme.__colorPalettes?.[value]
+  },
+}
 
 export const color: Config = {
   color: t.colors("color"),
   textColor: t.colors("color"),
   fill: t.colors("fill"),
   stroke: t.colors("stroke"),
+  colorPalette: colorPalettePropConfig,
 }
 
 export interface ColorProps {
@@ -26,4 +33,9 @@ export interface ColorProps {
    * The CSS `stroke` property for icon svgs and paths
    */
   stroke?: Token<CSS.Property.Color, "colors">
+  /**
+   * Create CSS variables for each color value in the specified color palette
+   * Follow up with renaming colorSchemes to colorPalettes in the generated CLI typings
+   */
+  colorPalette?: Token<string, "colorSchemes">
 }
