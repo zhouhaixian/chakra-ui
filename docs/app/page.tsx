b5696b3e11e1d86d475008c3f4f9f053c7bbd3dc
anubra266 <anubra266@gmail.com>
2024-04-14 12:59:06 -0500

    chore(docs): add connect section

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -1,8 +1,17 @@
-import { Button, Flex, Icon, IconButton, Link, Span } from "@chakra-ui/react"
+import {
+  Button,
+  Flex,
+  Icon,
+  IconButton,
+  Link,
+  Span,
+  Stack,
+} from "@chakra-ui/react"
 import NextLink from "next/link"
 import { BsGithub } from "react-icons/bs"
 import { AdBanner } from "../components/chakra-pro/ad-banner"
 import { ColorModeToggle } from "../components/color-mode-toggle"
+import { CommunityGFX } from "../components/community-gfx"
 import { Logo, LogoIcon } from "../components/logo"
 
 export default async function Page() {
@@ -53,8 +62,41 @@ export default async function Page() {
           </Flex>
         </Flex>
       </Flex>
+      {/* Community */}
+      <Flex align="center" maxW="8xl" mx="auto" pl="12" justify="space-between">
+        <Stack px="8" gap="16">
+          <Stack gap="4">
+            <Stack gap="2">
+              <Span color="#12A594" fontWeight="semibold" fontSize="sm">
+                Connect
+              </Span>
+              <Span fontWeight="bold" fontSize="4xl">
+                Meet the people
+              </Span>
+            </Stack>
+            <Span color="#646464" fontWeight="medium" fontSize="lg">
+              Feel free to ask questions, report issues, and meet new people.
+            </Span>
+          </Stack>
+          <Button
+            size="lg"
+            colorPalette="teal"
+            rounded="4px"
+            px="7"
+            w="fit-content"
+            asChild
+          >
+            <Link href="https://discord.com/invite/chakra-ui" external unstyled>
+              Join the discord
+            </Link>
+          </Button>
+        </Stack>
+        <Flex>
+          <CommunityGFX />
+        </Flex>
+      </Flex>
       {/* Footer */}
-      <Flex align="center" py="6" maxW="8xl" mx="auto">
+      <Flex align="center" py="6" px="12" maxW="8xl" mx="auto">
         <Flex align="center" px="8" gap="8" flex="1">
           <Flex w="20" justify="center">
             <NextLink href="/">
