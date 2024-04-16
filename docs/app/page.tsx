82553c0129af2bee4feaaadcbb90b09ed96b7c62
anubra266 <anubra266@gmail.com>
2024-04-16 01:40:37 -0500

    chore(docs): add praise section

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -1,8 +1,11 @@
 import {
   AspectRatio,
   Avatar,
+  Box,
   Button,
   Flex,
+  Grid,
+  GridItem,
   Icon,
   IconButton,
   Link,
@@ -32,7 +35,7 @@ export default async function Page() {
   const stats = await getStats()
 
   return (
-    <>
+    <Box pos="relative">
       <AdBanner />
       {/* NavBar */}
       <Flex align="center" py="6" maxW="8xl" mx="auto">
@@ -79,94 +82,92 @@ export default async function Page() {
         </Flex>
       </Flex>
       {/* Case studies */}
-      <Flex align="center" maxW="8xl" mx="auto" px="12">
-        <Stack px="8" gap="16" flex="1">
-          <Stack gap="16">
-            <Stack gap="4">
-              <Stack gap="2">
-                <Span color="#12A594" fontWeight="semibold" fontSize="sm">
-                  Case Studies
-                </Span>
-                <Span fontWeight="bold" fontSize="4xl">
-                  Built with Chakra
-                </Span>
-              </Stack>
-              <Span color="#646464" fontWeight="medium" fontSize="lg">
-                Your project can look as good as these! Check them out
+      <Stack px="8" pt="32" gap="16" flex="1" maxW="8xl" mx="auto">
+        <Stack gap="16">
+          <Stack gap="4">
+            <Stack gap="2">
+              <Span color="#12A594" fontWeight="semibold" fontSize="sm">
+                Case Studies
+              </Span>
+              <Span fontWeight="bold" fontSize="4xl">
+                Built with Chakra
               </Span>
             </Stack>
-            <Button
-              size="lg"
-              colorPalette="teal"
-              rounded="4px"
-              px="7"
-              w="fit-content"
-              asChild
-            >
-              <Link href="/showcase" external unstyled>
-                <span>View Showcases</span>
-                <Icon asChild>
-                  <HiOutlineArrowNarrowRight />
-                </Icon>
-              </Link>
-            </Button>
-            <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} gap="8" w="full">
-              {websites.map(({ name, image, url }) => (
-                <LinkBox
-                  cursor="pointer"
-                  pos="relative"
-                  key={url}
-                  shadow={{ base: "xs", _hover: "lg" }}
-                  transition="all 0.2s ease"
-                  rounded="lg"
-                  overflow="clip"
-                >
-                  <AspectRatio ratio={16 / 9} w="full">
-                    <Image
-                      height={200}
-                      width={300}
-                      alt={name}
-                      style={{ objectFit: "cover" }}
-                      src={
-                        image
-                          ? /^(https|http)/.test(image)
-                            ? image
-                            : `/${image}`
-                          : "/og-image.png"
-                      }
-                    />
-                  </AspectRatio>
-                  <LinkOverlay href={url} external>
-                    <Text
-                      fontWeight="semibold"
-                      textAlign="start"
-                      fontSize={{ base: "sm", md: "md" }}
-                      pos="absolute"
-                      bottom="4"
-                      right="4"
-                      shadow="md"
-                      rounded="full"
-                      display="flex"
-                      gap="3"
-                      alignItems="center"
-                      px="2"
-                      py="1.5"
-                      bg="bg"
-                    >
-                      {name}
-                      <Icon asChild boxSize="6">
-                        <HiOutlineArrowNarrowRight />
-                      </Icon>
-                    </Text>
-                  </LinkOverlay>
-                </LinkBox>
-              ))}
-            </SimpleGrid>
+            <Span color="#646464" fontWeight="medium" fontSize="lg">
+              Your project can look as good as these! Check them out
+            </Span>
           </Stack>
+          <Button
+            size="lg"
+            colorPalette="teal"
+            rounded="4px"
+            px="7"
+            w="fit-content"
+            asChild
+          >
+            <Link href="/showcase" external unstyled>
+              <span>View Showcases</span>
+              <Icon asChild>
+                <HiOutlineArrowNarrowRight />
+              </Icon>
+            </Link>
+          </Button>
+          <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} gap="8" w="full">
+            {websites.map(({ name, image, url }) => (
+              <LinkBox
+                cursor="pointer"
+                pos="relative"
+                key={url}
+                shadow={{ base: "xs", _hover: "lg" }}
+                transition="all 0.2s ease"
+                rounded="lg"
+                overflow="clip"
+              >
+                <AspectRatio ratio={16 / 9} w="full">
+                  <Image
+                    height={200}
+                    width={300}
+                    alt={name}
+                    style={{ objectFit: "cover" }}
+                    src={
+                      image
+                        ? /^(https|http)/.test(image)
+                          ? image
+                          : `/${image}`
+                        : "/og-image.png"
+                    }
+                  />
+                </AspectRatio>
+                <LinkOverlay href={url} external>
+                  <Text
+                    fontWeight="semibold"
+                    textAlign="start"
+                    fontSize={{ base: "sm", md: "md" }}
+                    pos="absolute"
+                    bottom="4"
+                    right="4"
+                    shadow="md"
+                    rounded="full"
+                    display="flex"
+                    gap="3"
+                    alignItems="center"
+                    px="2"
+                    py="1.5"
+                    bg="bg"
+                  >
+                    {name}
+                    <Icon asChild boxSize="6">
+                      <HiOutlineArrowNarrowRight />
+                    </Icon>
+                  </Text>
+                </LinkOverlay>
+              </LinkBox>
+            ))}
+          </SimpleGrid>
         </Stack>
-      </Flex>
+      </Stack>
       {/* Stats */}
-      <Flex maxW="8xl" mx="auto" px="12" direction="column">
+      <Flex maxW="8xl" mx="auto" pt="32" direction="column">
         <Stack px="8" py="32" gap="16" justify="center" flex="1">
           <Stack gap="4">
             <Stack gap="2">
@@ -245,54 +246,97 @@ export default async function Page() {
         <StatsGFX />
       </Flex>
       {/* Pro */}
-      <Flex align="center" maxW="8xl" mx="auto" px="12">
-        <Stack px="8" gap="16" flex="1">
-          <Stack gap="16" align="center" textAlign="center">
-            <Stack gap="4">
-              <Stack gap="2">
-                <Span color="#12A594" fontWeight="semibold" fontSize="sm">
-                  Premium
-                </Span>
-                <Span fontWeight="bold" fontSize="4xl">
-                  Go faster. Go Pro.
-                </Span>
-              </Stack>
-              <Span color="#646464" fontWeight="medium" fontSize="lg">
-                Beautiful and responsive React components to build your
-                application or marketing pages quicker.
+      <Stack px="8" pt="24" pb="16" gap="16" flex="1" maxW="8xl" mx="auto">
+        <Stack gap="16" align="center" textAlign="center">
+          <Stack gap="4">
+            <Stack gap="2">
+              <Span color="#12A594" fontWeight="semibold" fontSize="sm">
+                Premium
+              </Span>
+              <Span fontWeight="bold" fontSize="4xl">
+                Go faster. Go Pro.
               </Span>
             </Stack>
-            <Button
-              size="lg"
-              colorPalette="teal"
-              rounded="4px"
-              px="7"
-              w="fit-content"
-              asChild
-            >
-              <Link
-                href="https://discord.com/invite/chakra-ui"
-                external
-                unstyled
-              >
-                <span>Learn more</span>
-                <Icon asChild>
-                  <HiOutlineArrowNarrowRight />
-                </Icon>
-              </Link>
-            </Button>
-            <Image
-              src="/chakra-ui-ad.png"
-              alt="Chakra UI Pro Image"
-              layout="responsive"
-              width="1200"
-              height="320"
-            />
+            <Span color="#646464" fontWeight="medium" fontSize="lg">
+              Beautiful and responsive React components to build your
+              application or marketing pages quicker.
+            </Span>
           </Stack>
+          <Button
+            size="lg"
+            colorPalette="teal"
+            rounded="4px"
+            px="7"
+            w="fit-content"
+            asChild
+          >
+            <Link href="https://discord.com/invite/chakra-ui" external unstyled>
+              <span>Learn more</span>
+              <Icon asChild>
+                <HiOutlineArrowNarrowRight />
+              </Icon>
+            </Link>
+          </Button>
+          <Image
+            src="/chakra-ui-ad.png"
+            alt="Chakra UI Pro Image"
+            layout="responsive"
+            width="1200"
+            height="320"
+          />
         </Stack>
-      </Flex>
+      </Stack>
+      {/* Praise */}
+      <Stack px="8" gap="16" flex="1" maxW="8xl" mx="auto" py="24">
+        <Stack gap="16" align="center" textAlign="center">
+          <Stack gap="4">
+            <Stack gap="2">
+              <Span color="#12A594" fontWeight="semibold" fontSize="sm">
+                Praise
+              </Span>
+              <Span fontWeight="bold" fontSize="4xl">
+                Loved by product people like you
+              </Span>
+            </Stack>
+            <Span color="#646464" fontWeight="medium" fontSize="lg">
+              The proof is in the praise.
+            </Span>
+          </Stack>
+          <Grid
+            gap="2"
+            h="680px"
+            w="full"
+            templateColumns="repeat(3, 1fr)"
+            templateRows="repeat(4, 1fr)"
+          >
+            {/* Masonry grid */}
+            {Array.from({ length: 9 }).map((_, i) => {
+              const rows = {
+                0: 3,
+                1: 2,
+                2: 2,
+                3: 1,
+                4: 2,
+                5: 1,
+                6: 2,
+                7: 1,
+              } as any
+              return (
+                <GridItem key={i} rowSpan={rows[i]} border="solid 1px green">
+                  <div>{i}</div>
+                  <div>{i}</div>
+                  <div>{i}</div>
+                  <div>{i}</div>
+                  <div>{i}</div>
+                </GridItem>
+              )
+            })}
+          </Grid>
+        </Stack>
+      </Stack>
+
       {/* Community */}
-      <Flex align="center" maxW="8xl" mx="auto" pl="12" justify="space-between">
+      <Flex align="center" maxW="8xl" mx="auto" justify="space-between">
         <Stack px="8" gap="16">
           <Stack gap="4">
             <Stack gap="2">
@@ -325,30 +369,28 @@ export default async function Page() {
         </Flex>
       </Flex>
       {/* Footer */}
-      <Flex align="center" py="6" px="12" maxW="8xl" mx="auto">
-        <Flex align="center" px="8" gap="8" flex="1">
-          <Flex w="20" justify="center">
-            <NextLink href="/">
-              <LogoIcon />
-            </NextLink>
-          </Flex>
-          <Flex align="center" gap="6">
-            {["Product", "Company", "Resources"].map((l, i) => (
-              <Link key={i} fontWeight="semibold">
-                {l}
-              </Link>
-            ))}
-          </Flex>
-          <Flex flex="1" justify="end" align="center" gap="8">
-            {["Terms", "Privacy", "Cookies", "Contact"].map((l, i) => (
-              <Link key={i} fontWeight="semibold">
-                {l}
-              </Link>
-            ))}
-            <Span color="#838383">© 2024 Chakra UI. All rights reserved.</Span>
-          </Flex>
+      <Flex align="center" px="8" gap="8" flex="1" py="6" maxW="8xl" mx="auto">
+        <Flex w="20" justify="center">
+          <NextLink href="/">
+            <LogoIcon />
+          </NextLink>
+        </Flex>
+        <Flex align="center" gap="6">
+          {["Product", "Company", "Resources"].map((l, i) => (
+            <Link key={i} fontWeight="semibold">
+              {l}
+            </Link>
+          ))}
+        </Flex>
+        <Flex flex="1" justify="end" align="center" gap="8">
+          {["Terms", "Privacy", "Cookies", "Contact"].map((l, i) => (
+            <Link key={i} fontWeight="semibold">
+              {l}
+            </Link>
+          ))}
+          <Span color="#838383">© 2024 Chakra UI. All rights reserved.</Span>
         </Flex>
       </Flex>
-    </>
+    </Box>
   )
 }
